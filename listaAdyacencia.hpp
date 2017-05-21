6b32b06bd1ff7b5cf63657a24e2fc8a9df85dbd2
Adrián Castiglione <adriancastiglione@gmail.com>
2017-05-20 22:17:22 -0300

    lista de adyacencia

---
+++ listaAdyacencia.hpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <list>
+
+
+using namespace std;
+
+class listaAdyacencia<T>{
+
+private:
+
+	vector<list<T>> _nodos;
+	bool _indexadoEnCero;
+
+public:
+
+	listaAdyacencia(unsigned cantNodos, bool indexadoEnCero):
+		 _nodos(cantNodos, list<T>())
+		 _indexadoEnCero(indexadoEnCero){
+
+	}
+
+	void agregarAdyacente(unsigned nodo, T dato){
+		unsigned numNodo = nodo - _indexadoEnCero*1;
+		_nodos[numNodo].push_back(dato);
+	}
+
+	const list<T>& vecinos(unsigned nodo) const{
+		unsigned numNodo = nodo - _indexadoEnCero*1;
+		return _nodos[numNodo];
+	}
+}
